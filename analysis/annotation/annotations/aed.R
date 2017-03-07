1b014e64fa866ce510b57f7a1a53710e74b5a806
Adam Thrash <thrash@Shadow-login.HPC.MsState.Edu>
2017-03-07 14:47:27 -0600

    D8-8, D8-9, aed graphs

--- analysis/annotation/annotations/aed.R
+++
@@ -1,7 +0,0 @@
-
-args = commandArgs(trailingOnly=TRUE)
-data <- read.table(args[1])
-name <- args[2]
-library(ggplot2)
-ggplot(data, aes(V1))+stat_ecdf(geom="line")
-ggsave(paste(name,".aed.png", sep=""))
