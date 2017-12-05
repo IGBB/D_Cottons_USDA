3d8c6f226d6d787296c8dcef2fb14a1773df0c7c
Justin Conover <jconover@iastate.edu>
2017-12-05 15:29:10 -0600

    Fixed “proper” formatting (species order and sig figs). Code is messy, but works.

--- analysis/dNdS_CNV/DnDs.R
+++ analysis/dNdS_CNV/DnDs.R
@@ -3,6 +3,7 @@ library("ggplot2")
 library("dplyr")
 library("reshape2")
 library("gridExtra")
+library("data.table")
 
 ######
 #Read Data
@@ -23,7 +24,11 @@ filtered <- !(DF$X %in% DF[DF$dS > 0.6,]$X)
 DF <- DF[filtered,]
 
 
-
+Species_translate <- as.data.frame(c(levels(DF$qspecies), "D9-4"))
+colnames(Species_translate) <- c("Species")
+Species_translate$Species <- as.character(Species_translate$Species)
+Species_translate$latin <- c("G. thurberi ", "G. turneri", "G. schwendemanii","G. armourianum", "G. harknessii", "G. davidsonii","G. klotzschianum", "G. aridum", "G. raimondii","G. gossypioides", "G. lobatum", "G. trilobum", "G. laxum")
+Species_translate$order <- c("I", "D", "H", "B", "C", "K", "L", "E", "A", "M", "G", "J", "F")
 
 ######
 #Table
@@ -31,54 +36,56 @@ DF <- DF[filtered,]
 
 groups <- DF %>% group_by(`sspecies`, `qspecies`)
 dS.summary.table <- summarize(groups, LQ = quantile(dS, 0.25), Median = median(dS), UQ = quantile(dS, 0.75))
-levels(dS.summary.table$sspecies) <- c(levels(dS.summary.table$sspecies), "D1-35")
-levels(dS.summary.table$qspecies) <- c(levels(dS.summary.table$qspecies), "D9-4")
-dS.summary.table[nrow(dS.summary.table) + 1,] <- c("D1-35", "D1-35", NA, NA, NA)
-dS.summary.table[nrow(dS.summary.table) + 1,] <- c("D9-4", "D9-4", NA, NA, NA)
-dS.summary.table$IQR <- paste(dS.summary.table$LQ, dS.summary.table$UQ, sep="-")
-dS.summary.table$IQR <- paste("(", dS.summary.table$IQR, ")", sep="")
-dS.summary.table$concat <- paste(dS.summary.table$Median, dS.summary.table$IQR, sep = " ")
-dS.summary.df <- as.data.frame(t(acast(dS.summary.table, sspecies~qspecies, value.var = "concat")))
+levels(dS.summary.table$sspecies) <- c(levels(dS.summary.table$sspecies), "D1-35", levels(Species_translate$order))
+levels(dS.summary.table$qspecies) <- c(levels(dS.summary.table$qspecies), "D9-4", levels(Species_translate$order))
+dS.summary.table[nrow(dS.summary.table) + 1,] <- list("D1-35", "D1-35", NA, NA, NA)
+dS.summary.table[nrow(dS.summary.table) + 1,] <- list("D9-4", "D9-4", NA, NA, NA)
+dS.summary.table$sspecies <- Species_translate$order[match(dS.summary.table$sspecies, Species_translate$Species)]
+dS.summary.table$qspecies <- Species_translate$order[match(dS.summary.table$qspecies, Species_translate$Species)]
+dS.summary.table$IQR <- paste(format(dS.summary.table$LQ, digits = 3), format(dS.summary.table$UQ, digits = 3), sep="-")
+dS.summary.table$IQR <- paste("(", dS.summary.table$IQR,")", sep="")
+dS.summary.table$concat <- paste(format(dS.summary.table$Median, digits = 3), dS.summary.table$IQR, sep = " ")
+
+
+dS.summary.df <- as.matrix(t(acast(dS.summary.table, sspecies~qspecies, value.var = "concat")))
 dS.summary.df <- dS.summary.df[order(rownames(dS.summary.df)),order(colnames(dS.summary.df))]
-dS.summary.df[dS.summary.df=="NA (NA-NA)"] <- NA
+dS.summary.df <- as.data.frame(replace(dS.summary.df, is.na(dS.summary.df), t(dS.summary.df)[is.na(dS.summary.df)]))
+dS.summary.df[lower.tri(dS.summary.df, diag = T)] <- NA
+
+
+
+
+
+
 
 
 dN.summary.table <- summarize(groups, LQ = quantile(dN, 0.25), Median = median(dN), UQ = quantile(dN, 0.75))
-levels(dN.summary.table$sspecies) <- c(levels(dN.summary.table$sspecies), "D1-35","---")
-levels(dN.summary.table$qspecies) <- c(levels(dN.summary.table$qspecies), "D9-4","---")
-dN.summary.table[nrow(dN.summary.table) + 1,] <- c("D1-35", "D1-35", NA, NA, NA)
-dN.summary.table[nrow(dN.summary.table) + 1,] <- c("D9-4", "D9-4", NA, NA, NA)
-dN.summary.table$IQR <- paste(dN.summary.table$LQ, dN.summary.table$UQ, sep="-")
+levels(dN.summary.table$sspecies) <- c(levels(dN.summary.table$sspecies), "D1-35", levels(Species_translate$order))
+levels(dN.summary.table$qspecies) <- c(levels(dN.summary.table$qspecies), "D9-4", levels(Species_translate$order))
+dN.summary.table[nrow(dN.summary.table) + 1,] <- list("D1-35", "D1-35", NA, NA, NA)
+dN.summary.table[nrow(dN.summary.table) + 1,] <- list("D9-4", "D9-4", NA, NA, NA)
+dN.summary.table$sspecies <- Species_translate$order[match(dN.summary.table$sspecies, Species_translate$Species)]
+dN.summary.table$qspecies <- Species_translate$order[match(dN.summary.table$qspecies, Species_translate$Species)]
+dN.summary.table$IQR <- paste(format(dN.summary.table$LQ, digits = 3, drop0trailing = F), format(dN.summary.table$UQ, digits = 3), sep="-")
 dN.summary.table$IQR <- paste("(", dN.summary.table$IQR, ")", sep="")
-dN.summary.table$concat <- paste(dN.summary.table$Median, dN.summary.table$IQR, sep = " ")
-dN.summary.df <- as.data.frame(acast(dN.summary.table, sspecies~qspecies, value.var = "concat"))
+dN.summary.table$concat <- paste(format(dN.summary.table$Median, digits = 3), dN.summary.table$IQR, sep = " ")
+dN.summary.df <- as.matrix(acast(dN.summary.table, sspecies~qspecies, value.var = "concat"))
 dN.summary.df <- dN.summary.df[order(rownames(dN.summary.df)),order(colnames(dN.summary.df))]
-dN.summary.df[dN.summary.df=="NA (NA-NA)"] <- NA
 
-dN.summary.df <- data.frame(lapply(dN.summary.df, as.character), stringsAsFactors=FALSE)
-dS.summary.df <- data.frame(lapply(dS.summary.df, as.character), stringsAsFactors=FALSE)
 
-dN.summary.df[is.na(dN.summary.df)] <- "---" #THIS DOESN"T FREAKING WORK AND I DON"T KNOW WHY 
+dN.summary.df <- as.data.frame(replace(dN.summary.df, is.na(dN.summary.df), t(dN.summary.df)[is.na(dN.summary.df)]))
+dN.summary.df[upper.tri(dN.summary.df, diag = T)] <- NA
+
+
+dN.summary.df <- as.data.frame(lapply(dN.summary.df, as.character), stringsAsFactors=FALSE)
+dS.summary.df <- as.data.frame(lapply(dS.summary.df, as.character), stringsAsFactors=FALSE)
+
 Total.df <- replace(dS.summary.df, is.na(dS.summary.df), dN.summary.df[is.na(dS.summary.df)])
-colnames(Total.df) <- c("G. thurberi ", "G. turneri", "G. schwendemanii","G. armourianum", "G. harknessii", "G. davidsonii","G. klotzschianum", "G. aridum", "G. raimondii","G. gossypioides", "G. lobatum", "G. trilobum", "G. laxum")
+
+colnames(Total.df) <- Species_translate$latin[match(colnames(Total.df), Species_translate$order)]
 row.names(Total.df) <- colnames(Total.df)
 
-Df.melted <- melt(DF, id.vars = "Species Comparison", measure.vars = c("dS", "dN"))
-colnames(Df.melted)[3] <- c("Synonymous Substitution Rate")
 
-newDf.melted <- Df.melted[Df.melted$variable == "dS",]
-
-######
-#Plotting
-######
-
-
-#png("Table_dSDistribution.png", 5000, 5000, pointsize=12, res=600)
-#ggplot(newDf.melted, aes(newDf.melted$`Synonymous Substitution Rate`, colour = newDf.melted$`Species Comparison`)) + 
-#  geom_density(adjust = 1/5) + 
-#  theme(legend.position = c(0.6, 0.6), axis.title.y = element_blank(), legend.title = element_blank()) + 
-#  xlab("Synonymous Substitution Rate") 
-#dev.off()
 
 
 write.table(Total.df, "dNdS_table.txt", quote=F, sep="\t")
